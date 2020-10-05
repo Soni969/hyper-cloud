3ff00be7aa87dd6744369dcbe6108c5503173dc6
nishp77 <nishp77@gmail.com>
2020-10-05 11:35:27 -0500

    Client side ready for hyperdrive 😂😂

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Test extends React.Component {
-    render() {
-        return <h1>Hello World!</h1>;
-    }
-}
-
-ReactDOM.render(<Test />, document.getElementById("root"));
+import App from "./App";
 
 
-run().then((result) => console.log());
+ReactDOM.render(<App />, document.getElementById("root"));
 
-async function run() {
-    const SDK = window.datSDK;
-    const { Hyperdrive } = await SDK();
-    const archive = Hyperdrive("./first-drive", { persist: false });
-    await archive.ready();
-
-    const url = `dat://${archive.key.toString('hex')}`;
-    console.log(`Here's your URL: ${url}`);
-
-    archive.writeFile('/hello.txt', 'world', function (err) {
-        if (err) throw err
-        archive.readdir('/', function (err, list) {
-            if (err) throw err
-            console.log(list) // prints ['hello.txt']
-            archive.readFile('/hello.txt', 'utf-8', function (err, data) {
-                if (err) throw err
-                console.log(data) // prints 'world'
-            })
-        })
-    })
-
-
-
-};
